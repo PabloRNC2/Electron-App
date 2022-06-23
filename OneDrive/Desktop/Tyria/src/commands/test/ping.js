00b4f83f509376534afa1ab662e0a094bf2439ba
Pablo Ruano <pabloruanonavarro@gmail.com>
2022-06-23 12:03:48 +0200

    refractor(quest) Change history to story

--- OneDrive/Desktop/Tyria/src/commands/test/ping.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-module.exports = {
-	data: new SlashCommandBuilder().setName('ping').setDescription('🏓 Reply with pong!'),
-	async execute(interaction) {
-		await interaction.reply(':ping_pong: `' + interaction.client.ws.ping + '` ms.');
-	},
-};
