467cd7f0efe86f4fe6a54260f9a11234eba9e400
PabloRNC2 <109979236+PabloRNC2@users.noreply.github.com>
2022-07-25 18:42:30 +0200

    Delete OneDrive/Desktop/Aplicación Halverto Final directory

--- OneDrive/Desktop/Aplicación Halverto Final/src/deleteLog.js
+++
@@ -1,9 +0,0 @@
-async function deleteLog(){
-	const Constants = require('./Constants')
-	const { writeFileSync } = require('fs')
-	const LOG = Constants.LOG_FILE_PATH
-
-	await writeFileSync(LOG, '', 'utf-8')
-}
-
-module.exports = deleteLog
